fd75935a9c734bec3a9f289dc7f1136fb595bcb2
Philippe Lumpkin <phiglump@ut.utm.edu>
2019-03-14 10:39:06 -0500

    Finished Product

--- PiDarts/FindPiThread.cs
+++ PiDarts/FindPiThread.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PiDarts
@@ -14,13 +15,13 @@ namespace PiDarts
         int dartsToThrow = 0;
         int dartsInside = 0;
         Random newRandom;
-
+        //constructor for the darts to throw and the random generator
         public FindPiThread(int d)
         {
             dartsToThrow = d;
             newRandom = new Random();
         }
-
+        //accessor for the darts inside variable
         public int DartsInside
         {
             get
@@ -32,7 +33,11 @@ namespace PiDarts
                 dartsInside = value;
             }
         }
-
+        //The following will simulate the throwing of the darts.
+        //It will create an x and y to get coordinates for where it landed.
+        //Then the x and y are sent into a calculation to compare that to the number one to determine its location
+        //whether it would be inside or outside.
+        //if it is inside then it is added to the darts inside tally
         public void throwDarts()
         {
             for (int i = 0; i <= dartsToThrow; i++)
@@ -44,7 +49,7 @@ namespace PiDarts
 
                 if (z <= 1)
                 {
-                    dartsInside++;
+                    dartsInside = dartsInside + 1;
                 }
             }
         }
--- PiDarts/Program.cs
+++ PiDarts/Program.cs
@@ -14,16 +14,20 @@ namespace PiDarts
     {
         static void Main(string[] args)
         {
+            //The next four lines prompt the user to tell the program how many darts each thread should throw
+            //and how many threads should be ran.
             Console.WriteLine("What is the number of throws each thread should make?");
             int answer = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many threads would you like to perform?");
             int answer2 = Convert.ToInt32(Console.ReadLine());
-
+            //The two list are created here
             List<Thread> threadList = new List<Thread>(answer2);
             List<FindPiThread> findPiList = new List<FindPiThread>(answer2);
-
-            for (int i = 0; i <= answer2; i++)
+            //This will create the threads based on how many the client asked for
+            for (int i = 1; i <= answer2; i++)
             {
+                //creates and stores the threads 
+                //also starts them and sleeping them when finished.
                 FindPiThread findPiThread = new FindPiThread(answer);
                 findPiList.Add(findPiThread);
                 Thread myThread;
@@ -37,15 +41,23 @@ namespace PiDarts
             {
                 item.Join();
             }
+            double Temp = 0;
             foreach (FindPiThread item in findPiList)
             {
-                FindPiThread findPiThread = new FindPiThread(answer);
-                int darts = findPiThread.DartsInside;
+                //uses and accessor to get the number of darts that were thrown
+                double inside = item.DartsInside;
+                Console.WriteLine(item.DartsInside);
+                //Calculates each threads pi estimation and adds it to a temp holder 
+                double estimation = (inside / answer) * 4;
+                Temp = estimation + Temp;
+                if (item == findPiList.Last())
+                {
+                    //performs the calculation to estimate the value of pi based of the average pi estimation.
+                    estimation = Temp / answer2;
+                    Console.WriteLine("The pi estimation is: ");
+                    Console.WriteLine(estimation);
+                }
             }
-
-            double estimation = darts / answer * 4;
-            Console.WriteLine("The pi estimation is: ");
-            Console.WriteLine(estimation);
 
             Console.ReadKey();
         }
